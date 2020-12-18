0ea0a95c01e96d09ec5e8384c11a7a073e844e10
Youngju Jaden Kim <pydemia@gmail.com>
2020-12-18 15:06:11 +0900

    Update split_string_into_array_by_delimeter.sh

--- bash/split_string_into_array_by_delimeter.sh
+++ bash/split_string_into_array_by_delimeter.sh
@@ -2,9 +2,23 @@
 
 # string="Paris;France;Europe";
 string="Paris; France; Europe";
-delimeter=";"
-readarray -td "" arr < <(awk '{ gsub(/'$delimeter' */,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
-declare -p arr;
-for i in "${arr[@]}"; do
-  echo $i
-done
+# delimeter=";"
+
+# readarray -td "" arr < <(awk '{ gsub(/'$delimeter' */,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
+# # readarray -td "" arr < <(awk 'BEGIN {d="$delimeter "} { gsub(d,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
+# declare -p arr;
+# for i in "${arr[@]}"; do
+#   echo $i
+# done
+
+
+split() {
+  string=$1
+  delimeter=$2
+  
+  readarray -td "" arr < <(awk '{ gsub(/'$delimeter' */,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
+  declare -p arr;
+}
+
+res=$(split "$string" ";")
+echo $res

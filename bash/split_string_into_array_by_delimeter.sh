1584a36bd95ddbd5c7d024a15716b1b29ac74ef9
Youngju Jaden Kim <pydemia@gmail.com>
2020-12-18 14:59:48 +0900

    Update split_string_into_array_by_delimeter.sh

--- bash/split_string_into_array_by_delimeter.sh
+++ bash/split_string_into_array_by_delimeter.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-string="Paris,France,Europe"; string="Paris, France, Europe";
-delimeter=","
-readarray -td "" arr < <(awk '{ gsub(/, /,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
+# string="Paris;France;Europe";
+string="Paris; France; Europe";
+delimeter=";"
+readarray -td "" arr < <(awk '{ gsub(/'$delimeter' */,"\0"); print; }' <<<"${string}${delimeter} "); unset 'arr[-1]'; # declare -p arr;
+declare -p arr;
 for i in "${arr[@]}"; do
   echo $i
 done

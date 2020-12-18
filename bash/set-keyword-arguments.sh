26d9d15f023a47513cdb2e3f8a0bdbf0165acb67
Youngju Jaden Kim <pydemia@gmail.com>
2020-12-18 15:33:27 +0900

    Update set-keyword-arguments.sh

--- bash/set-keyword-arguments.sh
+++ bash/set-keyword-arguments.sh
@@ -4,27 +4,57 @@ var_nm1="predefined1"
 var_nm2="predefined2"
 var_nm3=""
 
-for i in "$@"; do
-  case $i in
-    -v=*|--pyversion=*)
-    PY_VER="${i#*=}"
-    shift # past argument=value
+echo "${var_nm1} / ${var_nm2} / ${var_nm3}"
+
+# "v,var_nm1" "a,var_nm2", "r,var_nm3"
+
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -v|--var_nm1)
+    var_nm1="$2"
+    shift 2 # past argument value
     ;;
-    -n=*|--env_name=*)
-    CONDA_ENV_NM="${i#*=}"
-    shift # past argument=value
+    -v*|-v=*|--var_nm1=*)
+    var_nm1="${1#*=}"
+    shift 1 # past argument=value
     ;;
-    -d=*|--display_name=*)
-    CONDA_DISP_NM="${i#*=}"
-    shift # past argument=value
+
+    -a|--var_nm2)
+    var_nm2="$2"
+    shift 2 # past argument value
     ;;
-    -e=*|--conda_yaml=*)
-    BASE_ENVFILE="${i#*=}"
-    shift # past argument=value
+    -a*|-a=*|--var_nm2=*)
+    var_nm2="${1#*=}"
+    shift 1 # past argument=value
     ;;
-    -r=*|--pip_req=*)
-    PIP_PKG_REQ="${i#*=}"
-    shift # past argument=value
+
+    -r|--var_nm3)
+    var_nm3="$2"
+    shift 2 # past argument value
     ;;
+    -r*|-r=*|--var_nm3=*)
+    var_nm3="${1#*=}"
+    shift 1 # past argument=value
+    ;;
+
+    -h|--help)
+    echo "Description: "
+    echo "Usage:"
+    echo "  -v|--var_nm1"
+    echo "                : var_nm1 description"
+    echo ""
+    echo "  -a|--var_nm2"
+    echo "                : var_nm2 description"
+    echo ""
+    echo "  -r|--var_nm3"
+    echo "                : var_nm3 description"
+    echo ""
+    exit 0;;
+
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) echo "Error: missing argument(s)"; break;;
+
   esac
 done
+
+echo "${var_nm1} / ${var_nm2} / ${var_nm3}"

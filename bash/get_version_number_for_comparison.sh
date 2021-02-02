46853cb474f44e3ecc0c0b128faee40a1fe0f9af
Youngju Jaden Kim <pydemia@gmail.com>
2021-02-03 02:42:01 +0900

    Update get_version_number_for_comparison.sh

--- bash/get_version_number_for_comparison.sh
+++ bash/get_version_number_for_comparison.sh
@@ -2,12 +2,20 @@ function get_version_num () {
   echo "$@" | grep -oP '[^vV].*'
 }
 
+function trunc_patchnum () {
+  echo "$@" | grep -oP '[0-9]+.[0-9]+'
+}
+
 function version () {
   get_version_num "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
+
+
 echo $(get_version_num v1.20.0)  # 1.20.0
 echo $(get_version_num V1.20.0)  # 1.20.0
+
+echo $(trunc_patchnum v1.20.0)  # 1.20
 
 echo $(version v1.20.0)  # 1020000000
 echo $(version V1.20.0)  # 1020000000

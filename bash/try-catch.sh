efadc55ecd71424fbadd5f7e665b6583ca00649d
Youngju Jaden Kim <pydemia@gmail.com>
2021-02-03 18:59:57 +0900

    Update try-catch.sh

--- bash/try-catch.sh
+++ bash/try-catch.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-function try()
-{
-    [[ $- = *e* ]]; SAVED_OPT_E=$?
-    set +e
+function try () {
+  [[ $- = *e* ]]; SAVED_OPT_E=$?
+  set +e
 }
 
-function throw()
-{
-    exit $1
+function throw () {
+  exit $1
 }
 
-function catch()
-{
-    export ex_code=$?
-    (( $SAVED_OPT_E )) && set +e
-    return $ex_code
+function catch () {
+  export ex_code=$?
+  (( $SAVED_OPT_E )) && set +e
+  return $ex_code
 }
 
-function throwErrors()
-{
-    set -e
+function throwErrors () {
+  set -e
 }
 
-function ignoreErrors()
-{
-    set +e
+function ignoreErrors () {
+  set +e
 }

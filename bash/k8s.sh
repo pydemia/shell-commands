ea60e6b7aaeec6dbe70baa8890a6ae9d2858cae1
Youngju Jaden Kim <pydemia@gmail.com>
2021-05-26 03:47:40 +0900

    Update k8s.sh

--- bash/k8s.sh
+++ bash/k8s.sh
@@ -40,10 +40,46 @@ function kexec () {
   kubectl -n ${NAMESPACE} exec --stdin --tty "$(kubectl -n ${NAMESPACE} get pods -l ${SELECTOR} -o jsonpath="{.items[0].metadata.name}")" -- ${CMD}
 }
 
+# "kubectl -n istio-system get gw ingress-prd -o jsonpath='{.metadata.annotations.kubectl\.kubernetes\.io/last-applied-configuration}' | yq -y"
+function kx () {
+  NAMESPACE="$1"
+  # SELECTOR="model=skh-bch-003"
+  SELECTOR="$2"
+  CMD="${3:-/bin/bash}"
+  kubectl -n ${NAMESPACE} exec --stdin --tty "$(kubectl -n ${NAMESPACE} get pods -l ${SELECTOR} -o jsonpath="{.items[0].metadata.name}")" -- ${CMD}
+}
+
+function kxc () {
+  NAMESPACE="$1"
+  # SELECTOR="model=skh-bch-003"
+  SELECTOR="$2"
+  CONTAINER="$3"
+  CMD="${4:-/bin/bash}"
+  kubectl -n ${NAMESPACE} exec --stdin --tty "$(kubectl -n ${NAMESPACE} get pods -l ${SELECTOR} -o jsonpath='{.items[0].metadata.name}')" -c ${CONTAINER} -- ${CMD}
+}
+
+function kxp () {
+  NAMESPACE="$1"
+  # SELECTOR="model=skh-bch-003"
+  PODNAME="$2"
+  CONTAINER="$3"
+  CMD="${4:-/bin/bash}"
+  kubectl -n ${NAMESPACE} exec --stdin --tty ${PODNAME} -c ${CONTAINER} -- ${CMD}
+}
+
 function kdb () {
   NAMESPACE="${1:-default}"
-  kubectl -n ${NAMESPACE} run --rm -it debug --image=pydemia/debug --restart=Never
+  SA="${2:default}"
+  kubectl -n ${NAMESPACE} run --rm -it debug --image=pydemia/debug --restart=Never --serviceaccount=${SA}
 }
+
+function kdbs () {
+  NAMESPACE="${1:-default}"
+  SA="${2:default}"
+  kubectl -n ${NAMESPACE} run --rm -it debug --image=pydemia/debug-slim --restart=Never --serviceaccount=${SA}
+}
+
+
 
 "kubectl get svc -n airuntime -o jsonpath='{range .items[*]}{.metadata.name}{"\t\t"}{.spec.ports}{"\n"}{end}'"
 

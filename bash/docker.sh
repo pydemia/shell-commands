00d592dfab93bdf49b8b4882c24d7745eb39ca84
Youngju Jaden Kim <pydemia@gmail.com>
2025-04-07 14:31:19 +0900

    Update docker.sh

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-alias docker_rm_untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}") && docker image prune -f'
+#alias docker_rm_untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}") && docker image prune -f'
 alias docker_rm_after_build='docker images -f "dangling=true" -q'
+alias docker_rm_untagged='docker rmi $(docker images -a --filter=dangling=true -q)'
 alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
 alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
 
